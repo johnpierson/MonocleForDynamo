4440c82168f0719991223166620fc9f0fc04e461
john pierson <sixtysecondrevit@gmail.com>
2020-06-01 10:35:56 -0500

    OS of Sticky notes

---
+++ src/StickyNotes/StickyNotesViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Dynamo.Core;
+using Dynamo.Extensions;
+using Dynamo.Graph.Nodes;
+
+namespace MonocleViewExtension.StickyNotes
+{
+    public class StickyNotesViewModel : NotificationObject, IDisposable
+    {
+        
+        private ReadyParams readyParams;
+
+
+        public StickyNotesViewModel(ReadyParams p)
+        {
+            readyParams = p;
+            //p.CurrentWorkspaceModel.NodeAdded += CurrentWorkspaceModel_NodesChanged;
+            //p.CurrentWorkspaceModel.NodeRemoved += CurrentWorkspaceModel_NodesChanged;
+        }
+
+        private void CurrentWorkspaceModel_NodesChanged(NodeModel obj)
+        {
+            RaisePropertyChanged();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
