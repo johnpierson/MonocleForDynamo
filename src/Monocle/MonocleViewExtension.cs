06b5c373ea1072d925e25e64a3ea191da3062676
John <sixtysecondrevit@gmail.com>
2022-09-16 09:16:15 -0600

    Closes #37

--- src/Monocle/MonocleViewExtension.cs
+++ src/Monocle/MonocleViewExtension.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Reflection;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -30,6 +31,28 @@ namespace MonocleViewExtension
 
         public void Startup(ViewStartupParams viewStartupParams)
         {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomainOnAssemblyResolve;
+        }
+
+        private Assembly CurrentDomainOnAssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            // Get assembly name
+            var assemblyName = new AssemblyName(args.Name).Name + ".dll";
+
+            // Get resource name
+            var resourceName = Assembly.GetExecutingAssembly().GetManifestResourceNames().Where(x => x.EndsWith(".dll")).ToArray().FirstOrDefault(x => x.EndsWith(assemblyName));
+            if (resourceName == null)
+            {
+                return null;
+            }
+
+            // Load assembly from resource
+            using (var stream = Globals.ExecutingAssembly.GetManifestResourceStream(resourceName))
+            {
+                var bytes = new byte[stream.Length];
+                stream.Read(bytes, 0, bytes.Length);
+                return Assembly.Load(bytes);
+            }
         }
 
         public void Loaded(ViewLoadedParams p)
