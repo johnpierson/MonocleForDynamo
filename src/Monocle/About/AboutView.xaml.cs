c191d6d122a84707359df490385e4bd04a71c9f0
John <sixtysecondrevit@gmail.com>
2023-09-14 09:48:27 -0600

    add spanish to resources
    Co-Authored-By: NonicaTeam <hello@nonica.io>

--- src/Monocle/About/AboutView.xaml.cs
+++ src/Monocle/About/AboutView.xaml.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Diagnostics;
-using System.Linq;
+﻿using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Dynamo.Controls;
-using Dynamo.Views;
-using MonocleViewExtension.Utilities;
 
 namespace MonocleViewExtension.About
 {
