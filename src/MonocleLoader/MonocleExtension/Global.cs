8d68f7f6dd2639dba4a0fa42626b639f3139d5b8
John <sixtysecondrevit@gmail.com>
2024-03-14 16:03:28 -0600

    Extension (loader) Created

---
+++ src/MonocleLoader/MonocleExtension/Global.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonocleExtension
+{
+    internal class Global
+    {
+        internal static Assembly ExecutingAssembly = Assembly.GetExecutingAssembly();
+        internal static string[] EmbeddedLibraries =
+            ExecutingAssembly.GetManifestResourceNames().Where(x => x.EndsWith(".dll")).ToArray();
+
+        internal static string MonocleForNet48 => EmbeddedLibraries.First(l => l.Contains("Monocle48"));
+        internal static string MonocleForNet8 => EmbeddedLibraries.First(l => l.Contains("Monocle8"));
+
+        internal static string PackageBinFolder => Path.GetDirectoryName(ExecutingAssembly.Location);
+        internal static string PackageExtraFolder => PackageBinFolder.Replace("bin", "extra");
+        internal static string PackageRoot => PackageBinFolder.Replace("bin", "");
+        internal static string MonocleViewExtensionDll => Path.Combine(PackageBinFolder, "MonocleViewExtension.dll");
+
+        internal static string ViewExtensionXml => Path.Combine(PackageExtraFolder, "Monocle_ViewExtensionDefinition.xml");
+        internal static string ViewExtensionXmlText => "<ViewExtensionDefinition>\r\n  <AssemblyPath>..\\bin\\MonocleViewExtension.dll</AssemblyPath>\r\n  <TypeName>MonocleViewExtension.MonocleViewExtension</TypeName>\r\n</ViewExtensionDefinition>\r\n";
+
+
+        internal static Version DynamoVersion { get; set; }
+        internal static Version DotNet8Version => new Version(3, 0, 0);
+
+    }
+}
---
+++ src/MonocleLoader/MonocleExtension/MonocleExtension.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Dynamo.Extensions;
+
+namespace MonocleExtension
+{
+    public class MonocleExtension : IExtension
+    {
+        public bool ReadyCalled = false;
+        public string UniqueId => "53301BE8-BDA9-47CA-9EF0-2B70808B13A5";
+        public string Name => "MonocleExtension";
+
+
+        public void Ready(ReadyParams rp)
+        {
+           this.ReadyCalled = true;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void Startup(StartupParams sp)
+        {
+            //only try to run this if the view extension DLL is missing (first run after install)
+            if (!File.Exists(Global.MonocleViewExtensionDll))
+            {
+                //check which version of Dynamo core is loaded
+                var dynamoCore = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(a => a.FullName.Contains("DynamoCore"));
+
+                Global.DynamoVersion = dynamoCore.GetName().Version;
+
+                //if we are in a 3.0 version or higher, load the .net8 version of monocle
+                if (Global.DynamoVersion.CompareTo(Global.DotNet8Version) >= 0)
+                {
+                    using (var stream = Global.ExecutingAssembly.GetManifestResourceStream(Global.MonocleForNet8))
+                    {
+                        var bytes = new byte[stream.Length];
+                        stream.Read(bytes, 0, bytes.Length);
+
+                        File.WriteAllBytes(Global.MonocleViewExtensionDll, bytes);
+                    }
+                }
+                //otherwise load the .net4.8 version
+                else
+                {
+                    using (var stream = Global.ExecutingAssembly.GetManifestResourceStream(Global.MonocleForNet48))
+                    {
+                        var bytes = new byte[stream.Length];
+                        stream.Read(bytes, 0, bytes.Length);
+
+                        File.WriteAllBytes(Global.MonocleViewExtensionDll, bytes);
+                    }
+                }
+
+                //write the view extension XML
+                File.WriteAllText(Global.ViewExtensionXml, Global.ViewExtensionXmlText);
+            }
+        }
+
+      
+
+        public void Shutdown()
+        {
+        }
+
+       
+    }
+}
